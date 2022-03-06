1247bed6a80413f25089d41ef9f577c3cc77b90c
Dharana Sharma <88919299+Dharana-Sharma@users.noreply.github.com>
2022-03-07 02:04:20 +0530

    Create Merge Two Sorted Lists.cpp

---
+++ Milestone 3/Merge Two Sorted Lists.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL)return l2;
+        if(l2==NULL)return l1;
+        if(l1->val > l2->val)
+        {
+          swap(l1,l2);
+        }
+        ListNode *ans = l1;
+        while(l1 != NULL && l2!=NULL)
+        {
+            ListNode *tmp=NULL;
+            while(l1 != NULL && l1->val<=l2->val)
+            {
+                tmp=l1;
+                l1=l1->next;
+            }
+            tmp->next=l2;
+            //swap
+          swap(l1,l2);
+        }
+        return ans;
+    }
+};
